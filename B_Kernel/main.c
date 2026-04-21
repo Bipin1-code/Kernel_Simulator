74376fa7d358920a6dc3367036218c8a0098ad7d
Bipin Karki <blueclub2205@gmail.com>
2026-04-21 13:33:20 +0530

    MMIO access validation rule is now established

--- B_Kernel/main.c
+++ B_Kernel/main.c
@@ -3,18 +3,17 @@
 #include "pci_logic.h"
 #include "OS_device.h"
 #include "basic_driver.h"
-
 #include <stdio.h>
 
 int main(){
     puts("********KERNEL MAIN:********");
 
-    printf("\n[SETUP] Initializing PCI simulator...\n");
+    puts("\n[SETUP] Initializing PCI simulator...\n");
     PciDevicePool();
     DriverPool();
+    PciSetDeviceCallback(OS_HandleNewPciDevice);
     OS_InitPciCallbacks();
     DriverFrameworkInit();
-    PciSetDeviceCallback(OS_HandleNewPciDevice);
     PciEnumerate();
     
     return 0;
